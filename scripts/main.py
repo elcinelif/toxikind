f856e6fdcf5e80ecf2d296001640b0ca2aeee535
Bart Dutkiewicz <dutkiewicz@mail.de>
2025-06-12 11:47:57 +0200

    (Feature:) Refactored feature transformation

--- scripts/main.py
+++ scripts/main.py
@@ -1,53 +1,73 @@
 """
 This script is to:
-- fast recreate scaled data if needed
-- fast retrain and reevaluate the current production model for comparison
-  with models in development
+- (Re)Fit and save the feature scaler
+- (Re)Create and save the preprocessed data
+- Fast retrain of the production model for comparison
+- Fast evaluation of the production model for comparison
 """
+
+# OS I/O
+import pickle
 
 # Data handling
 import pandas as pd
 
 # Internal modules
-from scale_features import scale_features
+from processing import fit_feature_scaler, transform_features
 
-def preprocess_train_data() -> None:
+def fit_save_feature_scaler(path_X_train_raw, path_feature_scaler) -> None:
     """
-    Purpose: preprocess raw training data (if needed)
-    Steps:
-    - Load raw training data
-    - Scale (minmax) features using "scale_features.py"
-    - Save processed training data in different folder
-    """
-    # Features
-    path_features_train = "raw_data/tox21_dense_train.csv.gz"
-    X_train_raw = pd.read_csv(path_features_train).set_index("Unnamed: 0")
-    X_train = scale_features(X_train_raw)
-    X_train.to_csv("data/X_train.csv")
+    This is a wrapper for "processing.fit_feature_scaler".
+    It loads raw feature training data from given path,
+    calls "processing.train_feature_scaler" and
+    saves the trained scaler as .pickle.
 
-    # Targets
-    path_targets_train = "raw_data/tox21_labels_train.csv.gz"
-    y_train = pd.read_csv(path_targets_train).set_index("Unnamed: 0")
-    y_train.to_csv("data/y_train.csv")
-
-def preprocess_test_data() -> None:
+    It assumes the raw data index column being unnamed.
     """
-    Purpose: preprocess raw testing data (if needed)
-    Steps:
-    - Load raw testing data
-    - Scale (minmax) features
-    - Save processed testing data in different folder
-    """
-    # Features
-    path_features_test = "raw_data/tox21_dense_test.csv.gz"
-    X_test_raw = pd.read_csv(path_features_test).set_index("Unnamed: 0")
-    X_test = scale_features(X_test_raw)
-    X_test.to_csv("data/X_test.csv")
+    # Laad raw feature training data from disk and set index
+    X_train_raw = pd.read_csv(path_X_train_raw).set_index("Unnamed: 0")
 
-    # Targets
-    path_targets_test = "raw_data/tox21_labels_test.csv.gz"
-    y_test = pd.read_csv(path_targets_test).set_index("Unnamed: 0")
-    y_test.to_csv("data/y_test.csv")
+    # Fit feature scaler
+    feature_scaler = fit_feature_scaler(X_train_raw)
+
+    # Save fitted scaler
+    path_feature_scaler = f"{path_feature_scaler}/feature_scaler.pickle"
+    with open(path_feature_scaler, "wb") as file:
+        pickle.dump(feature_scaler, file)
+
+    return None
+
+def load_transform_save_features(path_feature_scaler, path_x_raw, path_x) -> None:
+    """
+    This is a wrapper for "processing.transform_features".
+    It loads a fitted scaler from given path, raw feature data,
+    calls "processing.transform_features" and
+    saves the transformed data as .csv.
+
+    It assumes the raw data index column being unnamed.
+    """
+    # Load feature_scaler
+    path_feature_scaler = f"{path_feature_scaler}/feature_scaler.pickle"
+    with open(path_feature_scaler, "rb") as file:
+        feature_scaler = pickle.load(file)
+
+    # Load data
+    X_raw = pd.read_csv(path_x_raw).set_index("Unnamed: 0")
+
+    # Transform data
+    X = transform_features(X_raw, feature_scaler)
+
+    # Save data
+    X.to_csv(path_x)
+
+def load_save_targets(path_y_raw, path_y) -> None:
+    """
+    This function loads raw target data from given path, renames
+    and saves it to given path.
+    """
+    y_train = pd.read_csv(path_y_raw).set_index("Unnamed: 0")
+    y_train.to_csv(path_y)
+    return None
 
 def retrain_production_model() -> None:
     """
@@ -60,8 +80,7 @@ def retrain_production_model() -> None:
     """
     pass
 
-
-def reevaluate_production_model()-> dict:
+def evaluate_production_model()-> dict:
     """
     - Load retrained/refitted production model
     - Load processed testing data
@@ -70,8 +89,36 @@ def reevaluate_production_model()-> dict:
     """
     pass
 
-if __name__ == '__main__':
-    preprocess_train_data()
-    preprocess_test_data()
+if __name__ == "__main__":
+    # Fit/Save scaler
+    path_X_train_raw = "raw_data/tox21_dense_train.csv.gz"
+    path_feature_scaler = "production_model"
+    fit_save_feature_scaler(path_X_train_raw, path_feature_scaler)
+
+    # Load/Transform/Save training features
+    path_feature_scaler = "production_model"
+    path_x_raw = "raw_data/tox21_dense_train.csv.gz"
+    path_x = "data/X_train.csv"
+    load_transform_save_features(path_feature_scaler, path_x_raw, path_x)
+
+    # Load/Save training targets
+    path_y_raw = "raw_data/tox21_labels_train.csv.gz"
+    path_y = "data/y_train.csv"
+    load_save_targets(path_y_raw, path_y)
+
+    # Load/Transform/Save testing features
+    path_feature_scaler = "production_model"
+    path_x_raw = "raw_data/tox21_dense_test.csv.gz"
+    path_x = "data/X_test.csv"
+    load_transform_save_features(path_feature_scaler, path_x_raw, path_x)
+
+    # Load/Save testing targets
+    path_y_raw = "raw_data/tox21_labels_test.csv.gz"
+    path_y = "data/y_test.csv"
+    load_save_targets(path_y_raw, path_y)
+
+    # Retrain production model
     retrain_production_model()
-    reevaluate_production_model()
+
+    # Evaluate production model
+    evaluate_production_model()
--- scripts/processing.py
+++ scripts/processing.py
@@ -1,35 +1,16 @@
-"""
-TODO:
-rename file to "processing.py"
-
-split train from transform
-
-train_scaler
-    save trained scaler
-    train transformer on train data only
-    save transformer
-
-transformer in seperate function on test data and user data
-
-Index rename: ID
-"""
-
-# OS I/O
-import os
-
 # Data handling
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.compose import ColumnTransformer
 
-def scale_features(X: pd.DataFrame) -> pd.DataFrame:
+def fit_feature_scaler(X_train_raw: pd.DataFrame) -> Pipeline:
     """
-    This function scales feature tables with a MinMaxScaler trained on the input data
+    This function fits a MinMaxScaler with raw feature training data
     """
     # ColumnTransformer with MinMaxScaler
     column_transformer = ColumnTransformer([
-        ("minmax_scaler", MinMaxScaler(), X.columns)
+        ("minmax_scaler", MinMaxScaler(), X_train_raw.columns)
         ])
 
     # Pipeline
@@ -37,22 +18,22 @@ def scale_features(X: pd.DataFrame) -> pd.DataFrame:
         ("column_transformer", column_transformer)
         ])
 
-    # Transform data while preserving column names and index
-    X = pd.DataFrame(pipeline.fit_transform(X), columns=X.columns, index=X.index)
+    # Fit feature_scaler with raw feature training data
+    print("Fitting scaler...")
+    feature_scaler = pipeline.fit(X_train_raw)
 
-    print("✅ features processed.")
+    # Return fitted feature_scaler
+    print("✅ Scaler fitted")
+    return feature_scaler
 
+def transform_features(X_raw: pd.DataFrame, feature_scaler: Pipeline) -> pd.DataFrame:
+    """
+    This function transforms features using a scaler fitted on raw feature training data
+    """
+    # Transform features
+    print("Transforming features...")
+    X = pd.DataFrame(feature_scaler.transform(X_raw), columns=X_raw.columns, index=X_raw.index)
+
+    # Return transformed data
+    print("✅ Features transformed")
     return X
-
-if __name__ == '__main__':
-    # Load/Scale/Save training features
-    path_features_train = "raw_data/tox21_dense_train.csv.gz"
-    X_train_raw = pd.read_csv(path_features_train).set_index("Unnamed: 0")
-    X_train = scale_features(X_train_raw)
-    X_train.to_csv("data/X_train.csv")
-
-    # Load/Scale/Save testing features
-    path_features_test = "raw_data/tox21_dense_test.csv.gz"
-    X_test_raw = pd.read_csv(path_features_test).set_index("Unnamed: 0")
-    X_test = scale_features(X_test_raw)
-    X_test.to_csv("data/X_test.csv")
