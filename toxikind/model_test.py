07dd99a8b30ac0dde184ab2bf496d3a9d490fb61
pwd <pynev virtualenvs>
2025-06-16 07:33:35 +0300

    Created the model and model test files

---
+++ toxikind/model_test.py
@@ -0,0 +1,15 @@
+from model import load_model, predict_model
+import numpy as np
+import os
+
+# Construct the path relative to main.py
+model_path = os.path.join(os.path.dirname(__file__), "GradientBoosting.pkl")
+
+# Load the model
+model = load_model(model_path)
+X_sample = np.random.rand(1, 801)
+
+
+predictions = predict_model(model, X_sample)
+print(X_sample[0][0])
+print(predictions)
