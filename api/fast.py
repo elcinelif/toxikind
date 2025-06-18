dbc4f64585484629a7ccb176910fb6df570ca54d
ivan <ivankostov95@gmail.com>
2025-06-18 15:54:42 +0200

    added a table with all target predictions

--- api/fast.py
+++ api/fast.py
@@ -4,6 +4,69 @@ import pandas as pd
 import pickle
 from pydantic import BaseModel
 
+MODEL_INFO_DICT = {
+    'model_GBC_ahr': {
+        'abbreviation': 'NR-AhR',
+        'full_name': 'Aryl Hydrocarbon (NR-AhR)',
+        'threshold': 0.5
+    },
+    'model_GBC_ar': {
+        'abbreviation': 'NR-AR',
+        'full_name': 'Androgen Receptor (NR-AR)',
+        'threshold': 0.5
+    },
+    'model_GBC_are': {
+        'abbreviation': 'SR-ARE',
+        'full_name': 'Antioxidant Response (SR-ARE)',
+        'threshold': 0.5
+    },
+    'model_GBC_aromatase': {
+        'abbreviation': 'NR-Arom',
+        'full_name': 'Aromatase Inhibition (NR-Arom)',
+        'threshold': 0.5
+    },
+    'model_GBC_atad5': {
+        'abbreviation': 'SR-ATAD5',
+        'full_name': 'DNA Damage (SR-ATAD5)',
+        'threshold': 0.5
+    },
+    'model_GBC_er_lbd': {
+        'abbreviation': 'NR-ER-LBD',
+        'full_name': 'Estrogen Receptor LBD (NR-ER-LBD)',
+        'threshold': 0.5
+    },
+    'model_GBC_er': {
+        'abbreviation': 'NR-ER',
+        'full_name': 'Estrogen Receptor (NR-ER)',
+        'threshold': 0.5
+    },
+    'model_GBC_hse': {
+        'abbreviation': 'SR-HSE',
+        'full_name': 'Heat Shock Response (SR-HSE)',
+        'threshold': 0.5
+    },
+    'model_GBC_lbd': {
+        'abbreviation': 'NR-AR-LBD',
+        'full_name': 'Androgen Receptor LBD (NR-AR-LBD)',
+        'threshold': 0.5
+    },
+    'model_GBC_mmp': {
+        'abbreviation': 'SR-MMP',
+        'full_name': 'Mitochondrial Membrane (SR-MMP)',
+        'threshold': 0.5
+    },
+    'model_GBC_p53': {
+        'abbreviation': 'SR-p53',
+        'full_name': 'p53 Stress Response (SR-p53)',
+        'threshold': 0.5
+    },
+    'model_GBC_ppar_gamma': {
+        'abbreviation': 'NR-PPARγ',
+        'full_name': 'PPAR-gamma (NR-PPARγ)',
+        'threshold': 0.5
+    }
+}
+
 app = FastAPI()
 #app.state.model = load_model()
 
@@ -28,29 +91,22 @@ def predict(compound='Please type in a compound!'):
     if X_predict.shape[0] == 0:
         return 'Compound not found in Dataset'
 
-    model_list = ['fitted_xgb_are','fitted_xgb_mmp']
-    target_list = ['ARE', 'MMP']
-    predictions = []
-    predictions_probabilities = []
+    for model in MODEL_INFO_DICT.keys():
 
-    for i in model_list:
-
-        with open(f'production_model/{i}.pkl','rb') as f:
+        with open(f'production_model/{model}.pkl','rb') as f:
             trained_model= pickle.load(f)
 
-        prediction = trained_model.predict(X_predict)[0]
-        prediction_probability = f'{round(trained_model.predict_proba(X_predict)[:,-1][0]*100,2)}%'
+        prediction_probability = trained_model.predict_proba(X_predict)[:,-1][0]
+        MODEL_INFO_DICT[model]['toxicity_probability'] = round(prediction_probability,4)
+        threshold = MODEL_INFO_DICT[model]['threshold']
+        if prediction_probability > threshold:
+            MODEL_INFO_DICT[model]['Toxic?'] = 'Yes'
+        else:
+            MODEL_INFO_DICT[model]['Toxic?'] = 'No'
 
-        predictions.append(prediction)
-        predictions_probabilities.append(prediction_probability)
-
-    results_df = pd.DataFrame({
-    "target": target_list,
-    "prediction": predictions,
-    "probability": predictions_probabilities})
-
-    results_df['result'] = results_df['prediction'].apply(lambda x: 'Substance is Toxic' if x == 1 else 'Substance is not Toxic')
-    results_df.drop(columns='prediction', inplace=True)
+    results_df = pd.DataFrame(MODEL_INFO_DICT).transpose().reset_index()
+    results_df.drop(columns=['threshold','index'], inplace=True)
+    results_df.index += 1
     return results_df
 
 
